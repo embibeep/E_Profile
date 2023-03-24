600c49b44c8a1a93c8e74f5c3952d8a02bb94cc6
Nguyen Trong Ngoc <trongngocthanhchuong2001@gmail.com>
2023-03-25 00:54:04 +0700

    upload, first config

---
+++ src/views/main/App.js
@@ -0,0 +1,17 @@
+import logo from '../../assets/images/logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          Hello Huy!
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
